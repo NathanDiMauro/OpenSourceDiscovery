7c9d6b2ef42d80b47b2214269ca71aed92e1df03
Nathan.DiMauro <Nathan.DiMauro@asurion.com>
2021-06-09 15:36:34 -0400

    added express.js backend

--- client/src/components/pages/App.js
+++ client/src/components/pages/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import OpenCard from '../card'
 class App extends React.Component {
+  state = {
+    data: null
+  };
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+  }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <p className="App-intro">{this.state.data}</p>
         <OpenCard />
         <OpenCard />
         <OpenCard />
