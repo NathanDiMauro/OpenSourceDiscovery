62a14387367208292220cbb44f66b060c3631970
Nathan.DiMauro <Nathan.DiMauro@asurion.com>
2021-07-08 15:07:07 -0400

    Added a 404 page and route

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
 import App from './components/pages/App'
 import Projects from './components/pages/projects' 
 import Repo from './components/pages/repo' 
+import NotFound from './components/pages/notfound' 
 import Navigation from "./components/nav"
 
-const routing = (
+const Routs = () => (
   <Router>
   <Navigation />
+    <switch>
       <Route exact path="/" component={App} />
       <Route path="/projects" component={Projects} />
       <Route path="/repo/:repoName" component={Repo} />
+      <Route path="/404" component={NotFound} />
+      <Route path="*">
+          <Redirect to="/404" />
+      </Route>
+    </switch>
+
   </Router>
 )
 
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(<Routs />, document.getElementById('root'))
