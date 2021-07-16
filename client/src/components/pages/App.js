e6e80e53b3229d683d175a1739040ed77d79a80a
Nathan.DiMauro <Nathan.DiMauro@asurion.com>
2021-07-16 11:26:52 -0400

    added text to the home page

--- client/src/components/pages/App.js
+++ client/src/components/pages/App.js
@@ -4,6 +4,39 @@ class App extends React.Component {
     return (
       <div>
         <h1>Welcome!</h1>
+        <p>The goal of this project is to make it easier for Asurion internal employees to find extra projects to work. 
+          The work split at Asurion is 80-20 where 80% of an employees time is supposed to be spent on your day to day work. 
+          Whether that’s completely tickets, developing new features, QA testing, or monitoring deployments. The other 20 percent 
+          of your time can be spent working on other projects that aren’t directly related to your work. These projects have to help the 
+          company in some way. There have already been some amazing projects at the company that have come out of thew 80-20 split. Atom 
+          for instance has become a cornerstone of Wolfpack operations and was originally developed as a side project. These side projects 
+          also help avoid burnout and stress, by giving employees a chance to step away from everyday task and work on something else beneficial. 
+          It is currently hard to find out what open source projects are being worked on at Asurion. You either have ti hear about it from a 
+          coworker or happen to randomly stumble across it while searching through Jira. That is why I have developed this app. I wanted to make it 
+          easier for to find projects that might excite people. </p>
+          <h2>Current Features:</h2>
+            <ul>
+              <li>View all repos of an organization</li>
+              <li>Get a specific page of each repo</li>
+            </ul>
+          <h2>Next Features:</h2>
+            <ul>
+              <li>Better UI</li>
+              <ul>
+                <li>I am a backend engineer. Front end work gives me migraines and makes me want to throw my laptop out the window. Someone with 
+                  front end skills far better then mine could defiantly come in and clean up the UI</li>
+              </ul>
+              <li>Documentation</li>
+              <ul>
+                <li>This project has been worked on sporadically for the last 2 months. Documentation was not a priority at all. I simply wanted to 
+                  get the basic starting point of the project done and polish it into a presentable state.</li>
+              </ul>
+              <li>Search feature</li>
+              <ul>
+                <li></li>
+              </ul>
+            </ul>
+          
       </div>
     )
   }
